80d5add314422cc951008e558f684bcaecc6eef7
atmni5 <aaron.tumini@griffithuni.edu.au>
2017-03-12 14:15:35 +1000

    Final commit
    Completed test Android application

---
+++ app/src/main/java/com/example/windos/first/nextScreen.java
@@ -0,0 +1,13 @@
+package com.example.windos.first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nextScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_screen);
+    }
+}
